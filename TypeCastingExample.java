a1ef7697ae71d63bf8476aa6384c0cb505c8f03c
pankaj yadav <pankaj113786@gmail.com>
2025-12-27 11:56:19 +0530

    Add TypeCastingExample.java

---
+++ TypeCastingExample.java
@@ -0,0 +1,11 @@
+public class TypeCastingExample {
+    public static void main(String[] args) {
+        int Int = 9;
+        double Double = Int; //Automatic change integer into Double
+
+        System.out.println("Int value: " + Int);
+        System.out.println("Double value: " +Double);
+    }
+
+    
+}
